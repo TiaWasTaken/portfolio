bf10261a51afe8837f206fc0ca750ed0039f6158
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-28 18:16:35 +0200

    made the layout of contact, missing email

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
+  
+  const Earth = useGLTF('./planet/scene.gltf');
+
   return (
-    <div>Earth</div>
+    <primitive object={Earth.scene} scale={2.5}/>
   )
 }
 
-export default Earth
+
+const EarthCanvas = () => {
+  return (
+    <Canvas shadows frameloop='demand' gl={{ preserveDrawingBuffer: true }} camera={{ fov: 45, near: 0.1, far: 200, position:[-4, 3, 6] }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls autoRotate autoRotateSpeed={1.4} enableZoom={false} maxPolarAngle={Math.PI} minPolarAngle={-Math.PI} />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
