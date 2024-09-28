d439eb41893439557f5744b51ef390f53deb501c
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-28 16:10:43 +0200

    fixed balls bottom row not wokring

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,7 @@ import { technologies } from '../constants'
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10 pt-[70px]">
+    <div className="flex flex-row flex-wrap justify-center gap-10 pt-[70px] pb-[80px]">
       {technologies.map((technology) => (
         <div className="w-28 h-28 key={technology.name}">
           <BallCanvas icon={technology.icon}/>
