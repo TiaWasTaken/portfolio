3ce06ae4196a1bb3c2190cc5e6d9edf29934fb2c
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-28 14:36:56 +0200

    fixed transition to About Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
         <a href="#about" className="relative">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center cursor-pointer">
             <motion.div 
-              animate={{ y: [0, 24, 0] }} 
+              animate={{ y: [-10, 15, -10] }} 
               transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }} 
               className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
