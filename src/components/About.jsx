92b2fa2f36ccbf4586c9faa87a91650d899b2e6b
Mattia Arganetto <mattia@Macbook-di-Mattia.local>
2024-11-07 13:19:20 +0100

    prova ssh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 import { SectionWrapper } from '../hoc/index.js'
 
+
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
